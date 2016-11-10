ae884f52188060b2c0a457fac0067c47335e4cf5
Donald Hutchison <git@toastymofo.net>
2016-11-10 15:07:27 +0100

    Update apis to flixel v4.

--- StupidAwesomeBlackjack/source/PlayState.hx
+++ StupidAwesomeBlackjack/source/PlayState.hx
@@ -5,7 +5,7 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
-import flixel.util.FlxMath;
+import flixel.math.FlxMath;
 
 import game.rules.Outcome;
 import game.Shoe;
